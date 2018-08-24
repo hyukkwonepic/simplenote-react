a5d13b4f3d54a464164280ee5c65bf7b259e3c13
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-08-24 17:12:54 +0900

    unregistered service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './global.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
