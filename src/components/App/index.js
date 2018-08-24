b739225048807239a363fb5db761c13ea07577ff
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-08-24 14:30:54 +0900

    refactor app structures

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="title">Hello World</div>
+    );
+  }
+}
+
+export default App;
