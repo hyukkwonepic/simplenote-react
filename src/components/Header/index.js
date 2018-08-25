468f6bbef82a294f8ce5d436f86be34858e7c723
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-08-25 18:59:02 +0900

    edit favicon and add logo image on header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './index.css';
+import logo from '../../logo.png';
 
 class Header extends React.Component {
   render() {
     const { onAddNote, onDeleteNote } = this.props;
     return (
       <div className="header">
-        <div className="title">Alex's 심플노트</div>
+        <div className="title">
+          <img className="logo" src={logo} />
+          <span>Alex's 심플노트</span>
+        </div>
         <div className="buttons">
           <button onClick={onAddNote}>추가</button>
           <button onClick={onDeleteNote}>삭제</button>
